ff668d64d7fb32dc10ce86fa6a872bf0ebab1f51
Michael Craun <michael.craun@gmail.com>
2020-12-06 15:33:16 -0500

    Migrating project to Github

--- Hide and Seek/ContentView.swift
+++ Hide and Seek/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            CardView(show: $show)
+            Text("Tap Me!")
+        }.onTapGesture {
+            self.show.toggle()
+        }
+    }
+}
+
+struct CardView: View {
+    @Binding var show: Bool
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: show ? 12 : 2)
+            .foregroundColor(show ? .pink : .red)
+            .frame(width: 300, height: 300)
+            .animation(.spring())
+            .offset(x: show ? 0 : 300)
+//            .rotationEffect(Angle(degrees: show ? 12 : -12))
+            .rotation3DEffect(Angle(degrees: -24), axis: (x: 10, y: 10, z: -8))
+            .shadow(radius: 12)
+            .blur(radius: show ? 0 : 2)
+//            .scaleEffect(show ? 2 : 1)
     }
 }
 
